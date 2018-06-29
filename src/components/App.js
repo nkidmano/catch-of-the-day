e033ee8ab642f5815e9fd319ebdc8311cfc68f0c
nkidmano <nobudy55@gmail.com>
2018-06-29 22:30:54 +0700

    added fishform

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,32 @@ import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
 
-const App = () => (
-  <div className="catch-of-the-day">
-    <div className="menu">
-      <Header tagline="Fresh Seadfood Market" />
-    </div>
-    <Order />
-    <Inventory />
-  </div>
-);
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of existing fishes state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to fishes varible by using Date.now()
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set new fishes object to state by using setState()
+    this.setState({ fishes });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seadfood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,5 +1,15 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
-const Inventory = () => <div className="inventory">Inventory!!!</div>;
+class Inventory extends React.Component {
+  render() {
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
+  }
+}
 
 export default Inventory;
