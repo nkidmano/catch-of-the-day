f04c947d971c347c1b7ff40407d17748bc01386f
nkidmano <nobudy55@gmail.com>
2018-07-04 00:20:32 +0700

    added data validation

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
@@ -10,6 +11,10 @@ class App extends React.Component {
   state = {
     fishes: {},
     order: {}
+  };
+
+  static propTypes = {
+    match: PropTypes.object
   };
 
   componentDidMount() {
@@ -27,7 +32,6 @@ class App extends React.Component {
   }
 
   componentDidUpdate() {
-    console.log(this.state.order);
     localStorage.setItem(
       this.props.match.params.storeId,
       JSON.stringify(this.state.order)
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,7 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { formatPrice } from "../helpers.js";
 
 class Fish extends React.Component {
+  static propTypes = {
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    addToOrder: PropTypes.func
+  };
+
   render() {
     const { image, name, price, status, desc } = this.props.details;
     const isAvailable = status === "available";
